9c368cf02c3b1e64db0de47c3ad4433598c534a0
Dane Williams <dwilli36@nd.edu>
2020-05-04 04:54:43 +0000

    hell yes

--- app.py
+++ app.py
@@ -8,11 +8,17 @@ import json
 import requests
 import re
 import csv
+import hashlib
+import string
+import sys
+import argparse
+import concurrent.futures
 
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 
 from flask import Flask, request
+
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot.trainers import UbuntuCorpusTrainer
@@ -27,12 +33,12 @@ bot = ChatBot(bot_name)
 prev_message = None
 message_count = 0
 
-with open("custom_training.json", "r") as f:
-    training_data = json.loads(f.read())
+f = open("custom_training.json", "r")
+training_data = json.loads(f.read())
 
-with open(".secrets", "r") as f:
-    secrets = json.loads(f.read())
-    bot_id = secrets['bot_id']
+f = open(".secrets", "r")
+secrets = json.loads(f.read())
+bot_id = secrets['bot_id']
 
 
 # Flask Setup
@@ -59,33 +65,27 @@ def Pork_Chop():
 
 # Functions
 
-def train_bot_list(data: list):
-
-    trainer = ListTrainer(bot)
-
-    for conversation in data:
-        trainer.train(conversation)
-
-
+# Training
 def train_bot_corpus():
 
     trainer = ChatterBotCorpusTrainer(bot)
     trainer.train('chatterbot.corpus.english')
 
-
-def train_bot_csv(path: str):
+def train_bot_csv(path: str, cores: int):
 
     csvfile = open(path)
-    conversation = flatten(csv.reader(csvfile))
-    train_bot_list(list(conversation))
+    conversation = list(flatten(csv.reader(csvfile)))
+    trainer = ListTrainer(bot)
+    
+    if cores > 1:
+        with concurrent.futures.ProcessPoolExecutor(cores) as executor:
+            executor.map(trainer.train, conversation)
+    else:
+        trainer.train(conversation)
 
 
-def flatten(sequence):
-    for iterable in sequence:
-        for element in iterable:
-            yield element
 
-
+# HTTP Reqs
 def reply(message: str):
     
     url = 'https://api.groupme.com/v3/bots/post'
@@ -99,22 +99,36 @@ def reply(message: str):
     json = urlopen(request).read().decode()
 
 
+# Utilities
 def bot_message(post_req):
     return post_req['sender_type'] == 'bot'
+
+def flatten(sequence):
+    for iterable in sequence:
+        for element in iterable:
+            yield element
 
 
 # Main Function
 
 def main():
-    
+
+    # Parse Args
+    parser = argparse.ArgumentParser(prog='hulk')
+    parser.add_argument('-c', '--cores', help='Number of CPU cores to use', type=int, default=1)
+    parser.add_argument('-t', '--train', help='Train bot from data', action = 'store_true')
+    parser.add_argument('-d', '--deploy', help='Deploy bot with flask on port 80', action = 'store_true')
+    args = parser.parse_args()
+
     # Training
-    train_bot_corpus()
-    train_bot_list(training_data)
-    train_bot_csv('coderquadszn2.csv')
-    train_bot_csv('eboard.csv')
+    if args.train:
+        #train_bot_corpus()
+        train_bot_csv('coderquadszn2.csv', args.cores)
+        train_bot_csv('eboard.csv', args.cores)
 
     # Run with flask
-    app.run(host='0.0.0.0', port=80)
+    if args.deploy:
+        app.run(host='0.0.0.0', port=80)
 
 
 # Run Main
