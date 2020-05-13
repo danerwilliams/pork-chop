3c8eb77b560e45fbf2f33694c98e922ce1642bf9
Dane Williams <dwilli36@nd.edu>
2020-05-12 19:27:27 -0700

    gitignore works

---
+++ modules/usage.py
@@ -0,0 +1,12 @@
+
+def usage_handler(message, bot_id):
+    usage = '''
+    Say pork chops name and he will respond. 
+
+    With auto reply enabled, pork chop will automatically reply when he's feeling confident, even if you don't say his name.
+
+    Boss pork chop around with these commands:
+    !usage - display available commands
+    '''
+    
+    return usage
