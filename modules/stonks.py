3989113840714a42ce5523e7e6d2f2fa43e1b248
Conor Murphy <conor.p.murphy52@gmail.com>
2020-05-16 22:14:54 -0500

    ready to pull

--- modules/stonks.py
+++ modules/stonks.py
@@ -17,27 +17,9 @@ def stonks_handler(message):
         return '!stonks <symbol>'
     
     stock_html = requests.get(url).text
-    price = re.search(r'data-reactid="50">[0-9]+\.[0-9]{2}</span>', stock_html)
-    change = re.search(r'data-reactid="51">(.*?)</span>', stock_html)
-    print(price) 
-    if price.group():
-        return '$' + price.group(1) + ' / ' + change.group(1)
-
-    return 'Could not find stock info'
-        
-
-def steemer_stonks(message):
-    url = 'https://finance.yahoo.com/quote/aapl'
-    
-    stock_html = requests.get(url).text
     price = re.search(r'currentPrice.*?({.*?})', stock_html)
     change = re.search(r'regularMarketChangePercent.*?({.*?})', stock_html)
     if price.group():
         price = json.loads(price.group(1))['raw']
         change = json.loads(change.group(1))['raw']
-        print('$' + str(price) + ' / ' + '{:.2f}'.format(change) + '%')
-    # scripts = json.loads(re.search(r'currentPrice.*?({.*?})', stock_html).group(1))
-
-
-    # scheme = scripts[0].json()
-steemer_stonks('conor')
+        return '$' + str(price) + ' / ' + '{:.2f}'.format(change) + '%'
