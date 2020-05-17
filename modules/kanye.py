c15d1554d3cb137ebfd386668eb8d48ac89c594c
Beniamino Green <ben@greendalba.com>
2020-05-17 12:35:07 -0700

    Using different API endpoint + formatted as block quote

--- modules/kanye.py
+++ modules/kanye.py
@@ -3,13 +3,12 @@ import requests
 def kanye_handler():
     '''Gives a random Kanye West quote.'''
 
-    url="https://api.kanye.rest/"
-
+    url="https://api.kanye.rest/?format=text"
     r = requests.get(url)
+
     if r.status_code == 200:
-        quote = r.json()['quote']
-        return f"'{quote}' - Kanye West"
+        return f">'{r.text}' \n- Kanye West"
     else:
-        return f"Error {r.status_code} - could not retrieve quote"
+        return f"Error {r.status_code}: could not retrieve quote"
 
 
